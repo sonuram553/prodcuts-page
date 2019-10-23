e99ce212917e595a251099d37b714d07ef4dd065
Sonu Ram <sonuram6960@gmail.com>
2019-10-23 12:29:32 +0530

    Update styles

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -5,17 +5,16 @@ import { Injectable } from "@angular/core";
 })
 export class ProductsService {
   products = [
-    { productName: "Abc", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
-    { productName: "Abc", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
-    { productName: "Abc", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
-    { productName: "Abc", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
-    { productName: "Abc", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
-    { productName: "Abc", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
-    { productName: "Abc", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
-    { productName: "Abc", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
-    { productName: "Abc", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
-    { productName: "Abc", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
-    { productName: "Abc", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } }
+    { productName: "Tshirt 1", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
+    { productName: "Tshirst 2", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
+    { productName: "Tshirt 3", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
+    { productName: "Tshirt 4", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
+    { productName: "Tshirt 5", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
+    { productName: "Tshirt 6", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
+    { productName: "Tshirt 7", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
+    { productName: "Tshirt 8", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
+    { productName: "Tshirt 9", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
+    { productName: "Tshirt 10", inStock: true, sizes: { s: 5, m: 6, l: 3, xl: 4 } },
   ];
 
   constructor() {}
